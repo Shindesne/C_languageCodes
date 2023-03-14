4ce84c0db2a1da52e9cd9482a091606b633fa8a3
Snehal Shinde <90593105+Shindesne@users.noreply.github.com>
2023-03-14 12:28:24 +0530

    Create 3_Substraction_of_two_numbers.c

---
+++ 3_Substraction_of_two_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c;
+printf("enter the first number");
+scanf("%d",&a);
+printf("enter the second number");
+scanf("%d",&b);
+c=a-b;
+printf("the substraction of %d and %d is = %d",a,b,c);
+return 0;
+}
