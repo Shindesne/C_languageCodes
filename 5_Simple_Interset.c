8e6c26c95eba657eb0b8905b025b4d3832d707eb
Snehal Shinde <90593105+Shindesne@users.noreply.github.com>
2023-03-14 13:03:06 +0530

    Create 5_Simple_Interset.c

---
+++ 5_Simple_Interset.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c,si;
+    printf("enter the value for a,b,c");
+    scanf("%d%d%d",&a,&b,&c);
+    si=a*b*c/100;
+    printf("simple interest=%d",si);
+}
