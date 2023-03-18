23e5bc3abcee2c571f1f8f8baeb7a614585a79b8
Snehal Shinde <90593105+Shindesne@users.noreply.github.com>
2023-03-19 00:11:58 +0530

    Create 31_plus_pattern.c

---
+++ 31_plus_pattern.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,j;
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=5;j++)
+        {
+            if((i==3)||(j==3))
+            {
+            printf("x");
+         }
+        else
+        {
+        printf("  ");
+    }
+}
+    printf("\n");
+}
+}
