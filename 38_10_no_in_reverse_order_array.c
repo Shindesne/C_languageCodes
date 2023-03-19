8adc8f7149ceb1dac1279b4c067b898c7588e5dd
Snehal Shinde <90593105+Shindesne@users.noreply.github.com>
2023-03-20 00:00:50 +0530

    Create 38_10_no_in_reverse_order_array.c

---
+++ 38_10_no_in_reverse_order_array.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int Ar[10],i;
+    for(i=0;i<10;i++)
+    {
+        scanf("%d",&Ar[i]);
+    }
+    for(i=9;i>=0;i--)
+    {
+        printf("%d",Ar[i]);
+    }
+    }
