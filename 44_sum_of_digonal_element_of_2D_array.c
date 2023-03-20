9ffd19f596a5dd68d46a2f22b44172d45411eb46
Snehal Shinde <90593105+Shindesne@users.noreply.github.com>
2023-03-20 12:27:44 +0530

    Create 44_sum_of_digonal_element_of_2D_array.c

---
+++ 44_sum_of_digonal_element_of_2D_array.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[3][3],i,j,sum=0;
+   printf("enter the value");
+   for(i=0;i<3;i++)
+   {
+       for(j=0;j<3;j++)
+       {
+           scanf("%d",&a[i][j]);
+       }
+   }
+   printf("matrix a\n");
+   for(i=0;i<3;i++)
+   {
+       for(j=0;j<3;j++);
+       {
+           printf("%d\t",a[i][j]);
+           if(i==j)
+           {
+           sum=sum+a[i][j];
+           }
+       }
+       printf("\n");
+   }
+       printf("sum=%d",sum);
+       }
+
+
