26de0b652dc7e242d5e520ec84d96fdd296b9b4c
Snehal Shinde <90593105+Shindesne@users.noreply.github.com>
2023-03-21 23:07:56 +0530

    Create 52_(strlen)_length_of_string.c

---
+++ 52_(strlen)_length_of_string.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+    char str1[20];
+    printf("entre the string");
+    scanf("%s",str1);
+    printf("length of string is%d",strlen(str1));
+}
+
